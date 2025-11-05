bde2e9c085ea20ac1572191407285ffb75ad0b8a
JassonPerezAlvarez <jassongeme23j@gmail.com>
2025-11-05 10:41:11 -0600

    correccion

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dlst/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
 
@@ -9,3 +9,6 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
+
